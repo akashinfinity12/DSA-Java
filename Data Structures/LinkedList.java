5e883ecfad1b4aaf7e0920521703da4e7bc4430e
akash <akashudsouza12@gmail.com>
2022-10-08 12:25:25 +0530

    80per done

--- Data Structures/LinkedList.java
+++ Data Structures/LinkedList.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class LinkedList {
 
   // head of the linked list
@@ -23,7 +25,7 @@ class LinkedList {
       System.out.print(temp.data + " ");
       temp = temp.next;
     }
-
+    System.out.println();
   }
 
   // insert element at the end of linked list
@@ -44,23 +46,97 @@ class LinkedList {
     return list;
   }
 
-  public static void deleteByKey(LinkedList list, int key) {
+  // delete element by key
+  public static LinkedList deleteByKey(LinkedList list, int key) {
     Node temp = list.head;
+    Node prev = null;
 
     if (temp != null && temp.data == key) {
+      System.out.println("Found " + temp.data + " and deleted");
       list.head = temp.next;
+      return list;
     }
+
+    while (temp != null && temp.data != key) {
+      prev = temp;
+      temp = temp.next;
+    }
+
+    if (temp != null) {
+      System.out.println("Found " + temp.data + " and deleted");
+      prev.next = temp.next;
+    } else {
+      System.out.println("Key not found");
+    }
+
+    return list;
+  }
+
+  // delete by position of element
+  public static LinkedList deleteByPosition(LinkedList list, int position) {
+    Node temp = list.head;
+    Node prev = null;
+    int counter = 0;
+
+    if (temp != null && position == 0) {
+      System.out.println("Deleted element at index " + position);
+      list.head = temp.next;
+      return list;
+    }
+
+    while (temp != null && counter != position) {
+      prev = temp;
+      temp = temp.next;
+      counter++;
+    }
+
+    if (temp != null) {
+      prev.next = temp.next;
+      System.out.println("Deleted element at index " + position);
+    } else {
+      System.out.println("Index not present in the list");
+    }
+
+    return list;
 
   }
 
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     LinkedList l1 = new LinkedList();
 
-    l1 = insertEnd(l1, 2);
-    l1 = insertEnd(l1, 4);
+    int option, value;
 
-    printLinkedList(l1);
+    do {
+      System.out.println(
+          "\nMENU\n1. Insert element\n2. Insert element at position\n3. Delete the element\n4. Delete by position\n5. Print LinkedList\n6. Exit");
+      System.out.print("Enter any one option: ");
+      option = sc.nextInt();
 
+      switch (option) {
+        case 1:
+          System.out.print("Enter the element: ");
+          value = sc.nextInt();
+          l1 = insertEnd(l1, value);
+          break;
+        case 3:
+          System.out.print("Enter the element to delete: ");
+          value = sc.nextInt();
+          l1 = deleteByKey(l1, value);
+          break;
+        case 4:
+          System.out.print("Enter the position: ");
+          value = sc.nextInt();
+          l1 = deleteByPosition(l1, value);
+          break;
+        case 5:
+          printLinkedList(l1);
+          break;
+        case 6:
+          break;
+      }
+
+    } while (option != 6);
+    sc.close();
   }
-
 }
